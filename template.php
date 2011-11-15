76df0647a5c3ae5007941b1e937a02360e36efa5
Christopher Torgalson <bedlamhotel@gmail.com>
2011-11-14 20:30:40 -0800

    bootstrap-ize base files

--- template.php
+++ template.php
@@ -2,48 +2,21 @@
 
 /**
  * @file
- * Provides preprocess logic and other utilities to Spinoza and child themes.
+ * Provides preprocess logic and other utilities to Bootstrap and child themes.
  */
 
 
 /**
  * 
  */
-function spinoza_html_head_alter(&$head_elements) {
-  //kpr($head_elements);
-} // spinoza_html_head_alter()
+function bootstrap_html_head_alter(&$head_elements) {
+  
+} // bootstrap_html_head_alter()
 
 
 /**
  * Implements template_preprocess_html();
  */
-function spinoza_preprocess_html(&$variables, $hook) {
-  // Get path to the parent theme--we need it for all we're going to do:
-  $path_to_spinoza = drupal_get_path('theme', 'spinoza');
-  /*
-  // Add the less file with a link element:
-  $element = array(
-    '#tag' => 'link',
-    '#attributes' => array(
-      'href' => $path_to_spinoza . '/style.less', 
-      'rel' => 'stylesheet',
-      'type' => 'text/css',
-    ),
-  );
-  drupal_add_html_head($element, 'spinoza_style_less');
-  */
+function bootstrap_preprocess_html(&$variables, $hook) {
   
-  //drupal_add_css($path_to_spinoza . '/style.less', array('group' => CSS_THEME));
-
-  // Decide whether or not to add less.js to the theme:
-  $less_js_setting = theme_get_setting('spinoza_use_less_js');
-  switch ($less_js_setting) {
-    case 'theme-less':
-      drupal_add_js($path_to_spinoza . theme_get_setting('spinoza_less_js_theme_path'));
-      break;
-    case 'cdn-less':
-      drupal_add_js(theme_get_setting('spinoza_less_js_cdn_url'));
-      break;
-    default:
-  }  
-} // spinoza_preprocess_html()
+} // bootstrap_preprocess_html()
--- theme-settings.php
+++ theme-settings.php
@@ -9,30 +9,6 @@
 /**
  * Implements hook_form_system_theme_settings_alter().
  */
-function spinoza_form_system_theme_settings_alter(&$form, &$form_state) {
-  // Less css fieldset
-  $form['spinoza_less_js'] = array(
-    '#collapsed' => FALSE,
-    '#collapsible' => TRUE,
-    '#description' => t('Contains settings related to the use of Less css in this theme'),
-    '#title' => t('Less css settings'),
-    '#type' => 'fieldset',
-  );
-  $form['spinoza_less_js']['spinoza_use_less_js'] = array(
-    '#default_value' => theme_get_setting('spinoza_use_less_js'),
-    '#description' => t('<p>Use javascript to process *.less files in theme.</p><p><strong>Warning</strong>: visitors with javascript disabled will <em>not</em> see the fully-styled theme. It is strongly recommended to process *.less files on the server with nodejs or to convert *.less files to *.css files on the command line before putting the site into production. See !usage at lesscss.org for documentation.</p>', array('!usage' => l('"Usage" section', 'http://lesscss.org/#-client-side-usage'))),
-    '#options' => array(
-      'no-less' => t('Do not use less.js'),
-      'theme-less' => t('Use theme less.js'),
-      'cdn-less' => t('Use less.js from CDN'),
-    ),
-    '#title' => t('Javascript processing for *.less files'),
-    '#type' => 'radios',
-  );
-  $form['spinoza_less_js']['spinoza_less_js_cdn_url'] = array(
-    '#default_value' => theme_get_setting('spinoza_less_js_cdn_url'),
-    '#description' => t('Full URL to CDN location of less.js file.'),
-    '#title' => t('less.js CDN URL'),
-    '#type' => 'textfield',
-  );
-} // spinoza_form_system_theme_settings_alter()
+function bootstrap_form_system_theme_settings_alter(&$form, &$form_state) {
+  
+} // bootstrap_form_system_theme_settings_alter()
