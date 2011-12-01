fc7886ccb761f214086fe560a8a5b3e673704589
Christopher Torgalson <bedlamhotel@gmail.com>
2011-11-30 21:28:38 -0800

    Fix up styles, markup for area around "topbar"

--- page.tpl.php
+++ page.tpl.php
@@ -96,7 +96,7 @@
       <h2 class="title" id="page-title"><?php print $title; ?></h2>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
-      <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+      <?php if ($tabs['#primary'] != ''): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
       <?php print render($page['help']); ?>
       <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
       <?php print render($page['content']); ?>
